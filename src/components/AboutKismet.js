0ab4984ee5bf6fe61d9e631b90f5a11fae036bd7
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-07-13 22:17:35 -0400

    eod tues

--- src/components/AboutKismet.js
+++ src/components/AboutKismet.js
@@ -5,9 +5,15 @@ class AboutKismet extends Component {
 render() {
     return (
 
-<div>
-    <p>Kismet was created by a super cool person who doesn't have time to work on this page more</p>
-    <p>Contact Us Placeholder</p> 
+<div className="about_kismet">
+    <p className="about_info">
+    Kismet was created by a super cool person <br /> who doesn't have time to work on this page more
+    <br />  
+    <br /> 
+    Contact Us 
+    <br /> 
+    <img width="70px" src = "https://cdn.dribbble.com/users/4874/screenshots/2031709/messageincoming.gif"/>
+     </p> 
     </div>
 
         )
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,18 @@ class Home extends Component {
         return (
             <div className="homepage">
             <p className="home_sqaure">
-            <img className="home_img" src= 'https://i.pinimg.com/originals/8e/06/ea/8e06ea83cb664d889cc799b5d72f8330.jpg'/>
+            {/* <img className="home_img" src= 'https://i.pinimg.com/originals/8e/06/ea/8e06ea83cb664d889cc799b5d72f8330.jpg'/> */}
             <h1 className="home_1">Welcome to Kismet</h1>
             <br />  <br /> 
-            <h3 className="home_2">Birthdays, weddings, anniversaries, graduations. Select the perfect card for every event
+            <h3 className="home_2">Birthdays, weddings, anniversaries, graduations 
+            <br /> 
+            Select the perfect card for every event
+            <br /> 
             <br /> 
             Receive your Kismet Box and we will remind you when to sign & send each card
             <br /> 
+            <br /> 
+
             The people in your life can’t wait to hear from you</h3>
             <br /> 
             <h2 className="home_3">So. Who are we celebrating?</h2>
