12c9183ffed5f4b3565d44811ce2411ddf341d82
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-07-15 15:35:12 -0400

    css could be done + delete works

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { Card, CardTitle, CardSubtitle, CardGroup, CardBody, Button} from 'reactstrap';
 
-
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 const Subscription = (props) => {
+
+  let formatedMailDate = new Date(props.subscription.mail_by)
+  let formatedDate = new Date(props.subscription.date)
+  let formatedReminder = new Date(props.subscription.reminder_date)
 
   return (
 
     <div>
-            {/* {console.log(props.subscription.mail_by.toLocaleString('en-US', options))} */}
-            {/* {console.log(Date.parse(props.subscription.mail_by))} */}
+           {/* {console.log({props.subscription.mail_by}.toLocaleString('en-US', options))} */}
+             {/* {console.log(Date.parse(props.subscription.mail_by))} */}
             {/* {console.log(Date.parse(props.subscription.mail_by).toDateString())} */}
 
+{console.log(formatedDate)}
             <CardGroup className="text-center">
             <Card>  
             <CardTitle tag="h3">{props.subscription.event_name}</CardTitle> 
-            <CardSubtitle className="mb-2 text-muted">Event Date: {props.subscription.date.toLocaleString('en-US', options)}</CardSubtitle>
+            <CardSubtitle className="mb-2 text-muted">Event Date: {formatedDate.toDateString()}</CardSubtitle>
             <br />
             <CardBody>Repeat: {props.subscription.repeat}</CardBody>
             <br />
             <br />
 
-            <CardBody>Send By: {props.subscription.mail_by}</CardBody>
+            <CardBody>Send By: {formatedMailDate.toDateString()} </CardBody>
             <br />
-            <CardBody>Reminder Date: {props.subscription.reminder_date}</CardBody>
+            <CardBody>Reminder Date: {formatedReminder.toDateString()}</CardBody>
             <br />
 
-            <Button className="find_cart" onClick={event =>  window.location.href='/cards'}>Edit Event</Button> 
-            <Button onClick={() => props.handleDelete(props.subscription.id)}>Delete Event</Button> 
+            <Button className="edit_event" onClick={event =>  window.location.href='/cards'}>Edit Event</Button> 
+            <Button className="delete_event" onClick={() => props.handleDelete(props.subscription.id)}>Delete Event</Button> 
 
             </Card>
             </CardGroup>
